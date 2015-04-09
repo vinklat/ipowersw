d7a28e7c871c87dba23628a4d2b0d535808ac8a8
Václav Vinklát <vin@email.cz>
2015-04-09 23:20:50 +0200

    improved requests

--- ipowersw.ino
+++ ipowersw.ino
@@ -1,23 +1,21 @@
 #include <UIPEthernet.h>
-// The connection_data struct needs to be defined in an external file.
-//#include <UIPServer.h>
-//#include <UIPClient.h>
 
+/* 
+ * Setup your application here:
+ */
+ 
 #define DEBUG
-#define REQ_BUF_SIZE 5
-#define LOC_BUF_SIZE 10
 #define PIN_RELAY 4
 #define PIN_DHT 5
 
-#define IP_DHCP
+#define USE_DHCP
+#define IP_ADDR 10,28,17,123
+#define MAC_ADDR 0xDE,0xAD,0xBE,0xEF,0xFE,0xED
 
-byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
-};
+#define REQ_BUF_SIZE 5
+#define LOC_BUF_SIZE 10
 
-#ifndef IP_DHCP
-IPAddress ip(10, 28, 17, 123);
-#endif
+
 
 template<class T> inline Print &operator <<(Print &obj, T arg) { 
   obj.print(arg); return obj;
@@ -27,6 +25,11 @@ template<class T> inline Print &operator <<(Print &obj, T arg) {
 // with the IP address and port you want to use
 // (port 80 is default for HTTP):
 EthernetServer server(80);
+int relay_state;
+
+/*
+ *
+ */
 
 void setup() {
   #ifdef DEBUG
@@ -36,30 +39,39 @@ void setup() {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
   #endif
+
+  const byte mac[] = { MAC_ADDR };
   
   // start the Ethernet connection:
-  #ifdef IP_DHCP
+  #ifdef USE_DHCP
   if (Ethernet.begin(mac) == 0) {
+    #ifdef DEBUG
     Serial.println("Failed to configure Ethernet using DHCP");
+    #endif
     // no point in carrying on, so do nothing forevermore:
     for (;;)
       ;
   #else
+  IPAddress ip(IP_ADDR);
   Ethernet.begin(mac, ip);
   #endif
   }
   
   server.begin();
   #ifdef DEBUG
-  Serial.print("server is at ");
-  Serial.println(Ethernet.localIP());
+  Serial << "server is at " << Ethernet.localIP() << '\n';
   #endif
  
   // default relay pin state HIGH (=relay output LOW)
   pinMode(PIN_RELAY, OUTPUT);
   digitalWrite(PIN_RELAY, HIGH);
+  relay_state=0;
 }
 
+
+/*
+ *
+ */
 
 #define REQ_ERR 0
 #define REQ_GET 1
@@ -119,7 +131,7 @@ void get_req(EthernetClient *client, int *req, char *loc_buf) {
           break;
 
           case STATE_GET_LOC:
-            if (pos==LOC_BUF_SIZE-1 || c==' ')
+            if (pos==LOC_BUF_SIZE-1 || c==' ' || c=='\n' || c=='\r')
               c='\0';
             *(loc_buf+pos) = c;
             if (c=='\0') {
@@ -154,6 +166,113 @@ void get_req(EthernetClient *client, int *req, char *loc_buf) {
 }
 
 
+/*
+ *
+ */
+
+#define HTTP "HTTP/1.0 "
+#define REPLY_200 "200 OK\n"
+#define REPLY_400 "400 Bad Request\n"
+#define REPLY_404 "404 Not Found\n"
+#define API_CONTYPE "Content-Type: application/json\n"
+#define HTML_CONTYPE "Content-Type: text/html\n"
+#define CLOSE "Connection: close\n"
+#define NO_CACHE "Cache-Control: no-cache, no-store, must-revalidate\nPragma: no-cache\nExpires: 0\n"
+
+#define HTML_HEAD "<!DOCTYPE HTML><html><body>"
+#define HTML_FOOT "</body></html>"
+#define HTML_FORM_ON "<form action=\"/1\" method=\"POST\"><input type=\"submit\" value=\"Turn ON\"></form>"
+#define HTML_FORM_OFF "<form action=\"/0\" method=\"POST\"><input type=\"submit\" value=\"Turn OFF\"></form>"
+
+#define LOC_ROOT 0
+#define LOC_0 1
+#define LOC_1 2
+#define LOC_API_ROOT 3
+#define LOC_API_0 4
+#define LOC_API_1 5
+
+void render_response(EthernetClient *client, int *req, char *loc_buf) {
+  const char* const locations[] = {"/", "/0", "/1", "/api/", "/api/0", "/api/1" };
+  bool found=false;
+
+  int loc;  
+  for (loc=0; loc<6; loc++) {
+    if (!strcmp(loc_buf, locations[loc])) {
+      found=true;
+      break;
+    }
+  }
+  
+  if (!*req) {
+    *client << HTTP << REPLY_400 << HTML_CONTYPE << CLOSE << '\n';
+    *client << HTML_HEAD << REPLY_400 << HTML_FOOT;
+    return;
+  }
+   
+  if (found) 
+  {
+    if (*req==REQ_GET) {
+      switch (loc) {
+        case LOC_ROOT:
+          *client << HTTP << REPLY_200 << HTML_CONTYPE << NO_CACHE << CLOSE << '\n';
+          *client << HTML_HEAD << "relay: " << relay_state;
+          if (relay_state)
+            *client << HTML_FORM_OFF;
+          else
+            *client << HTML_FORM_ON;
+          *client << HTML_FOOT;
+          break;
+        case LOC_API_ROOT:
+          *client << HTTP << REPLY_200 << API_CONTYPE << CLOSE << '\n';
+          *client << "{\"relay\": " << relay_state << "}";
+          break;
+        default:
+          found=false;
+      }
+    }
+    
+    if (*req==REQ_POST) {
+      switch (loc) {
+        case LOC_API_0:
+          digitalWrite(PIN_RELAY, HIGH);
+          relay_state=0;
+          *client << HTTP << REPLY_200 << API_CONTYPE << CLOSE << '\n';
+          *client << "{\"relay\": " << relay_state << "}";
+          break;
+        case LOC_API_1:
+          digitalWrite(PIN_RELAY, LOW);
+          relay_state=1;
+          *client << HTTP << REPLY_200 << API_CONTYPE << CLOSE << '\n';
+          *client << "{\"relay\": " << relay_state << "}";
+          break;
+        case LOC_0:
+          digitalWrite(PIN_RELAY, HIGH);
+          relay_state=0;
+          *client << HTTP << REPLY_200 << HTML_CONTYPE << NO_CACHE << CLOSE << '\n';
+          *client << HTML_HEAD << "relay: " << relay_state << HTML_FORM_ON << HTML_FOOT;
+          break;
+        case LOC_1:
+          digitalWrite(PIN_RELAY, LOW);
+          relay_state=1;
+          *client << HTTP << REPLY_200 << HTML_CONTYPE << NO_CACHE << CLOSE << '\n';
+          *client << HTML_HEAD << "relay: " << relay_state << HTML_FORM_OFF << HTML_FOOT;
+          break;
+        default:
+          found=false;
+      }
+    }
+  }
+  
+  if (!found) {
+    *client << HTTP << REPLY_404 << HTML_CONTYPE << CLOSE << '\n';
+    *client << HTML_HEAD << REPLY_404 << HTML_FOOT;
+    return;
+  }
+}
+
+/*
+ *
+ */
 
 void loop() {
  char loc_buf[LOC_BUF_SIZE];
@@ -166,24 +285,7 @@ void loop() {
     #endif
 
     get_req(&client, &req, loc_buf);
-
-    if (req)
-    {
-      switch (req) {
-        case REQ_GET:
-          client << "request: " << req << ' ' << loc_buf << '\n';
-        break;
-        case REQ_POST:
-          if (!strcmp(loc_buf,"/1"))
-            digitalWrite(PIN_RELAY, LOW);
-          else
-            if (!strcmp(loc_buf,"/0"))
-              digitalWrite(PIN_RELAY, HIGH);
-              //todo else 404
-        break;             
-      }
-    }     
-    //todo else 404
+    render_response(&client, &req, loc_buf);
     
     // give the web browser time to receive the data
     delay(2);
